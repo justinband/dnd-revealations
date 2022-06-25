4e501dbb529e0190fb74dbcf61433ece471ff45f
unknown <jstinband@gmail.com>
2022-06-25 16:38:34 -0500

    client and server folders... the beginning of a webapp

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import io from 'socket.io-client';
 
+const socket = io.connect("http://localhost:3001");
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
--- server/index.js
+++ server/index.js
@@ -1,13 +1,27 @@
-
 const express = require('express');
-const http = require('http');
 const app = express();
-const server = http.createServer(app);
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
 
-app.get('/', (req, res) => {
-  res.send("hello world");
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
 });
 
-server.listen(3000, () => {
-  console.log("listening on *:3000");
+io.on("connection", (socket) => {
+  console.log(`New connection`);
+});
+
+// app.get('/', (req, res) => {
+//   res.send("hello world");
+// });
+
+server.listen(3001, () => {
+  console.log("listening on *:3001");
 });
